57be9ceea4e0fb2c3c0387117d49ef92dd3b9434
TimberTang <tangming0915@gmail.com>
2021-07-30 14:48:34 +0800

    fix

--- app/common/utils/sign.ts
+++ app/common/utils/sign.ts
@@ -1,19 +1,18 @@
-
-import crypto from 'crypto';
+import crypto from "crypto"
 
 function dataToString(args: any) {
-  let result = '';
-  const keys = Object.keys(args).sort();
+  let result = ""
+  const keys = Object.keys(args).sort()
   for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    if (result.length > 0) result = result + '&';
-    result = result + key + '=' + args[key];
+    const key = keys[i]
+    if (result.length > 0) result = result + "&"
+    result = result + key + "=" + args[key || ""]
   }
 
-  return result;
+  return result
 }
 
 export function sign(args: any, secret: string) {
-  const hex = crypto.createHmac('sha256', secret).update(dataToString(args)).digest('hex');
-  return Buffer.from(hex).toString('base64');
+  const hex = crypto.createHmac("sha256", secret).update(dataToString(args)).digest("hex")
+  return Buffer.from(hex).toString("base64")
 }
